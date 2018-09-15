e779d9b3dae7f5641f81f10be00480c0b32502cb
Daniel Tang <dt19@rice.edu>
2018-09-15 16:00:34 -0500

    added pages for mortgage, retirement, and car

--- financial_lit/app.py
+++ financial_lit/app.py
@@ -1,13 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
+
 app = Flask(__name__)
 
+logged_in = False # Change this depending on whether the user is logged in (for later)
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('home.html',login = logged_in)
 
 @app.route('/start')
 def start():
-    return render_template('start.html')
+    return render_template('start.html',login = logged_in)
+
+@app.route('/retirement')
+def retirement():
+    return render_template('retirement.html',login = logged_in)
+
+@app.route('/mortgage')
+def mortgage():
+    return render_template('mortgage.html',login = logged_in)
+
+@app.route('/mortgage', methods=['POST'])
+def mortgage_post():
+    text = request.form['sadsa']
+
+    return text
+
+@app.route('/car_payment')
+def car_payment():
+    return render_template('car_payment.html',login = logged_in)
 
 if __name__ == '__main__':
     app.run(debug=True)
