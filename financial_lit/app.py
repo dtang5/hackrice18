20dac1605f4a0f4d0d0ae31ae6e3975a2f992046
Daniel Tang <dt19@rice.edu>
2018-09-15 13:23:45 -0500

    Added start page

--- financial_lit/app.py
+++ financial_lit/app.py
@@ -2,8 +2,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-
 def index():
     return render_template('home.html')
+
+@app.route('/start')
+def start():
+    return render_template('start.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
